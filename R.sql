569f20b50c4ac38b764f5fee8e1e7ea3c12dfe32
stocklols <expiringsoon@hush.com>
2016-04-08 11:37:23 -0700

    On the database level

--- R.sql
+++ R.sql
@@ -1,11 +1,15 @@
+---- Database level:
+USE 
+GO
+
 
 ---- Overview:
 SELECT 
 	rt.name AS TableName
 	, rc.name AS Column_Name
 	, rt.is_replicated
-FROM DBA.sys.columns rc
-	INNER JOIN DBA.sys.tables rt ON rc.OBJECT_ID = rt.OBJECT_ID
+FROM sys.columns rc
+	INNER JOIN sys.tables rt ON rc.OBJECT_ID = rt.OBJECT_ID
 WHERE rt.is_ms_shipped = 0
 	AND rc.is_replicated = 1
 
@@ -15,8 +19,8 @@ WHERE rt.is_ms_shipped = 0
 SELECT 
 	rt.name AS TableName
 	, COUNT(rc.name) AS ColumnCount
-FROM DBA.sys.columns rc
-	INNER JOIN DBA.sys.tables rt ON rc.OBJECT_ID = rt.OBJECT_ID
+FROM sys.columns rc
+	INNER JOIN sys.tables rt ON rc.OBJECT_ID = rt.OBJECT_ID
 WHERE rt.is_ms_shipped = 0
 	AND rc.is_replicated = 1
 GROUP BY rt.name
